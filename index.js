12851c7c5caca856c9c1c17ce457e4b79b645a4a
Jacob Ebey <jacob.ebey@live.com>
2018-10-11 22:21:19 -0700

    Use Float32Array when defaulting out
    V8 gives you a nice little performance bump if you can set the length of an array during initialization and it does not have to figure out the type of the containing items. Plus, you are asking for a Float32Array in the docs as the output parameter.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function intersection (out, ro, rd, aabb) {
   if (d === Infinity) {
     out = null
   } else {
-    out = out || []
+    out = out || new Float32Array(3)
     for (var i = 0; i < ro.length; i++) {
       out[i] = ro[i] + rd[i] * d
     }
